afb8ce5dc7724a1ba313f3c573e83a6445348633
Eric Shapiro <shapiro@relium.com>
2021-12-13 23:54:11 -0500

    Initial Commit

---
+++ ExampleMacApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleMacApp
+//
+//  Created by Eric Shapiro on 12/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleMacApp/ExampleMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleMacAppApp.swift
+//  ExampleMacApp
+//
+//  Created by Eric Shapiro on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
